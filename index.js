a82200e3c5a0a255bfe5671dc9b320d693667e45
Jenifer29b <Jenifer29b@gmail.com>
2024-07-17 16:43:21 +0530

    first commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use((cors));
+app.use(cors());
 const PORT = 4000;
 
 
 
 app.get('/first', (req,res) => {
-    res.send(200).json({message: "App is listening and its working"})
+    res.status(200).json({ message: "App is listening and its working" });
 })
 
 
